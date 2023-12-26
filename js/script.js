8d863b2a33dd583040f0de02215a4de0ec98b0f2
Ryan Victor <ryanvictor.am@gmail.com>
2023-12-25 23:40:01 -0400

    feat(bicicletas): Adicionar função galeria de bicicletas

--- js/script.js
+++ js/script.js
@@ -36,7 +36,6 @@ function ativarPergunta(event){
   resposta.classList.toggle('ativa');
   const ativa = resposta.classList.contains('ativa');
   pergunta.setAttribute('aria-expanded', ativa)
-  console.log(pergunta)
 }
 
 function eventosPerguntas(pergunta) {
@@ -44,3 +43,23 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 1000px)').matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
